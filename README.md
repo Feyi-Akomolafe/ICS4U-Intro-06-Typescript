c4a08b282a69c47230dccdaeb268344097910b93
Debian <mofeyifoluwa.akomolafe@mths.ca>
2024-03-04 14:40:40 +0000

    Added Hello World Program

---
+++ README.md
@@ -0,0 +1,15 @@
+# yes
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run yes
+```
+
+This project was created using `bun init` in bun v1.0.29. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ index.ts
@@ -0,0 +1,13 @@
+/**
+ * * The HelloWorld program implements an application that
+ * * simply displays "Hello World!" to the standard output.
+ * *
+ * * By:      Feyi Akomolafe
+ * * Version: 1.0
+ * * Since:   2024-02-13
+ * */ 
+
+console.log("Hello, World!")
+
+console.log("\nDone.")
+
